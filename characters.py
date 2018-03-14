55d357775f8dcce59d07e35c268c022a4d27f218
ItsCodingNix <35217330+ItsCodingNix@users.noreply.github.com>
2018-03-14 15:36:34 +0100

    Update characters.py

--- characters.py
+++ characters.py
@@ -1,52 +1,53 @@
-import csv
-import random
-import texts
-import characters
-from characters import charactDict
+charactDict = {'warrior':{'hp':16, 'atk':2,'def':2}, 'mage':{'hp':12, 'atk':4,'def':0},'rogue':{'hp':14, 'atk':3,'def':1}}
 
-def choseEnemy():
-  enemiestats = {}
 
-  for row in fitxer:
-    enemiestats[row[0]] = {} 
-    enemiestats[row[0]]['atk']=row[1]
-    enemiestats[row[0]]['def']=row[2]
-    enemiestats[row[0]]['hp']=row[3]
-
-  enemies = list(enemiestats.keys())
-  random.shuffle(enemies)
-  enemyChosen = enemies[0]
-  enemy = characters.Character(enemiestats[enemyChosen], enemiestats[enemyChosen]['hp'], enemiestats[enemyChosen]['atk'], enemiestats[enemyChosen]['def'], 1 )
-  return enemy
-
-filename ="enemies.csv"
-lect = open(filename)
-fitxer = csv.reader(lect)
-inventory=[]
-
-badans = True
-
-theEnemy = choseEnemy()
-
-print ("Hello Adventurer, What is your Name?")
-crname = input("")
-
-texts.intro(crname)
-
-crclass = texts.chrType(charactDict)
-
-print ("Great!")
+class Character:
+    
+  def __init__(self, name, hp, attackPoints, defensePoints, level):
+    self.name = name
+    self.description = "This shape has not been described yet"
+    self.author = "Nobody has claimed to make this shape yet"
+    self.HP = hp
+    self.atk = attackPoints
+    self.defense = defensePoints
+    self.level = level
+    
+  def printStats(self):
+    strStats = 'HP = {}, atk = {}, def = {}'.format(self.HP,self.atk,self.defense)
+    print(strStats)
   
-'''atack, defence, base hp and level'''
+  def increaseLevel(self):
+    self.level = self.level + 1
 
-if crclass == "warrior" :
-  crstats = characters.Warrior(crname)
-elif crclass == "mage":
-  crstats = characters.Mage(crname)
-elif crclass == 'rogue':
-  crstats = characters.Rogue(crname)
-
-print('Wellcome {} the {}'.format(crname, crclass.capitalize()))
-crstats.printStats()
-
-texts.chosePath()
+class Warrior(Character):
+  
+  def __init__(self, name):
+    pro = 'warrior'
+    self.description = "This shape has not been described yet"
+    self.name = name
+    self.HP = charactDict[pro]['hp']
+    self.atk = charactDict[pro]['atk']
+    self.defense = charactDict[pro]['def']
+    self.level = 1
+    
+class Mage(Character):
+  
+  def __init__(self, name):
+    pro = 'mage'
+    self.description = "This shape has not been described yet"
+    self.name = name
+    self.HP = charactDict[pro]['hp']
+    self.atk = charactDict[pro]['atk']
+    self.defense = charactDict[pro]['def']
+    self.level = 1
+    
+class Rogue(Character):
+  
+  def __init__(self, name):
+    pro = 'rogue'
+    self.description = "This shape has not been described yet"
+    self.name = name
+    self.HP = charactDict[pro]['hp']
+    self.atk = charactDict[pro]['atk']
+    self.defense = charactDict[pro]['def']
+    self.level = 1
