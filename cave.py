db7257c9b08f7b5d37c670cffd84228c78b4d70b
ItsCodingNix <35217330+ItsCodingNix@users.noreply.github.com>
2018-03-19 10:18:24 +0100

    Create cave.py

---
+++ cave.py
@@ -0,0 +1,18 @@
+import colors
+
+def cave():
+  print(colors.yellow("very well young adventurer. so to pass, Master Ori (a weird bar tender) offers you the way if you answer his riddle:"))
+  print("")
+  riddleans=input(colors.cyan("the riddle is: what ios tall when its young and short when its old?:"))
+  badans = True  
+  while badans:
+    if riddleans == "candle":
+      print("very well. you now have entered the %s." % (colors.red('CAVE')))
+      break
+    else:
+      print("")
+      print("the answer is not correct you shall not be granted the %s." % (colors.red("WAY")))
+      print(" ")
+      print(" ")
+      riddleans=input(colors.cyan("the riddle is: what ios tall when its young and short when its old?:"))
+cave()
