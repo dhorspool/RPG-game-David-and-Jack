51d157a946bad2a7912bdccb9caf8558c809417e
ItsCodingNix <35217330+ItsCodingNix@users.noreply.github.com>
2018-03-20 11:19:29 +0100

    Update forest.py

--- forest.py
+++ forest.py
@@ -1,4 +1,7 @@
 import colors
+import random
+
+
 
 def forest():
   print(colors.yellow("very well young aprentice. so to pass, Master Elenoid (a weird basard) offers you the way if you answer his riddle:"))
@@ -15,5 +18,47 @@ def forest():
       print(" ")
       print(" ")
       riddleans=input(colors.cyan("the riddle is: what walks with four legs in the morning, two in the afternoon and three at night?:"))
+      
+
+def start_forest():
+  i = 0
+  enemies = ['MagicWolf','GiantBee','Rat','Dragon']
+  print ("")
+  print ("")
+  print ('w = walk foward')
+  print ('l = go left')
+  print ('r = go right')
+  while i < 3:
+    i=i+1
+    print("")
+    walking=input('What do you wanna do?')
+    if walking == "w":
+      print (colors.yellow('Perefect! You have moved 10 m forward.'))
+    elif walking == "l":
+      print ("you have successfully mooved to the left")
+    elif walking == 'r':
+      print ('you have now turned right')
+    else:
+      print ('you have wrote it wrong')
+      
+      
+  else:
+    print ("")
+    print (colors.red('You have encountered a wild beast!'))
+    random.shuffle(enemies)
+    print ('Its a: %s!' % (enemies[0]))
+    
+  
+
+  
+  
         
+
+
+
+
+
+
+
 forest()
+start_forest()
