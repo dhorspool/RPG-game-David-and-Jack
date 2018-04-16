980481f5b05b8bd935a5e8d56011ff10f6cb049d
dhorspool <35217363+dhorspool@users.noreply.github.com>
2018-04-16 09:37:00 +0200

    Update forest.py

--- forest.py
+++ forest.py
@@ -20,45 +20,6 @@ def forest():
       riddleans=input(colors.cyan("the riddle is: what walks with four legs in the morning, two in the afternoon and three at night?:"))
       
 
-def start_forest():
-  i = 0
-  enemies = ['MagicWolf','GiantBee','Rat','Dragon']
-  print ("")
-  print ("")
-  print ('w = walk foward')
-  print ('l = go left')
-  print ('r = go right')
-  while i < 3:
-    i=i+1
-    print("")
-    walking=input('What do you wanna do?')
-    if walking == "w":
-      print (colors.yellow('Perefect! You have moved 10 m forward.'))
-    elif walking == "l":
-      print ("you have successfully mooved to the left")
-    elif walking == 'r':
-      print ('you have now turned right')
-    else:
-      print ('you have wrote it wrong')
-      
-      
-  else:
-    print ("")
-    print (colors.red('You have encountered a wild beast!'))
-    random.shuffle(enemies)
-    print ('Its a: %s!' % (enemies[0]))
-    
-  
-
-  
-  
-        
-
-
-
-
-
-
+  import encounter
 
 forest()
-start_forest()
